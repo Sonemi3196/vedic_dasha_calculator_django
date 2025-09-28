0ad5231797632a3bd86824957b2398d6a88d200d
sonemi3196 <12f82dd_hex@ezweb.ne.jp>
2025-09-28 19:04:36 +0900

    ALLOWED_HOST

--- vedic_dasha_project/vedic_dasha_project/settings.py
+++ vedic_dasha_project/vedic_dasha_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hai%+==$ug7nm15h)&j$uaxz8m0l&@20ot)#^)+oiaaf4^=3(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sonemi3196.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
